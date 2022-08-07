88383ab598a4385aee49d0c08485b8496d75decc
hellfrozenrain <cholinesterasa@gmail.com>
2022-08-07 20:28:51 +0300

    lesson_2 dz first tyr

---
+++ my_todolist/frontend/src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuItem = ({menu}) => {
+    return (
+        <h1>Menu</h1>
+    )
+}
+
+export default MenuItem
--- my_todolist/my_todolist/settings.py
+++ my_todolist/my_todolist/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'Usersapp',
 ]
 
@@ -49,7 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127:0.0.1:3000",
+    "http://localhost:3001",
+    "http://127:0.0.1:3001",
+    ]
 
 ROOT_URLCONF = 'my_todolist.urls'
 
