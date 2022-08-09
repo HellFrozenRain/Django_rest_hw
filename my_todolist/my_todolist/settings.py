8c0bd67d917277937892151cc9cc9fc59c8659c9
hellfrozenrain <cholinesterasa@gmail.com>
2022-08-09 22:16:05 +0300

    lesson_2 dz

--- my_todolist/my_todolist/settings.py
+++ my_todolist/my_todolist/settings.py
@@ -45,13 +45,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
+
 
 ]
 CORS_ALLOWED_ORIGINS = [
@@ -135,5 +137,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Usersapp.MyUser'
-
-#not working
